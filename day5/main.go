cf2072a10669b0fbfbd471f969c5c15c5fb12160
Robert Chirinos <robert.chirinos@pucp.edu.pe>
2024-12-08 14:04:38 -0500

    Day5 Pt2 solution

--- day5/main.go
+++ day5/main.go
@@ -11,6 +11,7 @@ import (
 
 func Run() {
 	solvePt1()
+	solvePt2()
 }
 
 func solvePt1() {
@@ -21,13 +22,50 @@ func solvePt1() {
 	for i := 1; scanner.Scan(); i++ {
 		line := scanner.Text()
 		ocurrences, lineNums := readUpdate(line)
-		if evaluateUpdate(lineNums, ocurrences, rules) {
+		if correct, _, _ := evaluateUpdate(lineNums, ocurrences, rules); correct {
 			mid := lineNums[len(lineNums)/2]
 			sum += mid
 		}
 	}
 
 	fmt.Println("Pt1 sum is:", sum)
+}
+
+func solvePt2() {
+	scanner := util.Scanner("day5")
+	rules := readRules(scanner)
+
+	var sum int
+	for i := 1; scanner.Scan(); i++ {
+		line := scanner.Text()
+		ocurrences, lineNums := readUpdate(line)
+		if correct, posEval, errVal := evaluateUpdate(lineNums, ocurrences, rules); !correct {
+			sum += swap(lineNums, rules, ocurrences, posEval, errVal)
+		}
+	}
+
+	fmt.Println("Pt2 sum is:", sum)
+}
+
+func swap(nums []int, rules map[int][]int, ocurrences map[int]int, posEval, errVal int) int {
+	for correct := false; !correct; correct, posEval, errVal = evaluateUpdate(nums, ocurrences, rules) {
+		cp := ocurrences[errVal]
+		if cp == -1 {
+			cp = 0
+		}
+
+		ocurrences[errVal], ocurrences[nums[posEval]] = ocurrences[nums[posEval]], ocurrences[errVal]
+		nums[cp], nums[posEval] = nums[posEval], nums[cp]
+	}
+	return nums[len(nums)/2]
+}
+
+func copyMap(ocurrences map[int]int) map[int]int {
+	cp := make(map[int]int)
+	for key, val := range ocurrences {
+		cp[key] = val
+	}
+	return cp
 }
 
 func readRules(scanner *bufio.Scanner) (rules map[int][]int) {
@@ -59,14 +97,14 @@ func readUpdate(line string) (ocurrences map[int]int, lineNums []int) {
 	return
 }
 
-func evaluateUpdate(lineNums []int, ocurrences map[int]int, rules map[int][]int) bool {
+func evaluateUpdate(lineNums []int, ocurrences map[int]int, rules map[int][]int) (bool, int, int) {
 	for i, val := range lineNums {
 		require := rules[val]
 		for _, r := range require {
 			if ocurrences[r] < i && ocurrences[r] != 0 {
-				return false
+				return false, i, r
 			}
 		}
 	}
-	return true
+	return true, 0, 0
 }
