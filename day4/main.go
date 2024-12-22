5b557103890eac6bb58a107af05f99561a8d13f3
Robert Chirinos <robert.chirinos@pucp.edu.pe>
2024-12-22 15:23:35 -0500

    Day4 Pt2 solution
    finally

--- day4/main.go
+++ day4/main.go
@@ -7,38 +7,66 @@ import (
 )
 
 func Run() {
-	solvePt1()
+	puzzle := read()
+	var pt1Sum, pt2Sum int
+	for i := range puzzle {
+		for j, char := range puzzle[i] {
+			if char == 'X' {
+				pt1Sum += solvePt1(puzzle, i, j)
+			} else if char == 'A' {
+				pt2Sum += solvePt2(puzzle, i, j)
+			}
+		}
+	}
+	fmt.Println("Pt1 sum:", pt1Sum)
+	fmt.Println("Pt2 sum:", pt2Sum)
 }
 
-func solvePt1() {
-	var puzzle []string
+func solvePt2(puzzle []string, i, j int) (sum int) {
+	if i == 0 || i == len(puzzle)-1 || j == 0 || j == len(puzzle[0])-1 {
+		return
+	}
+	down := [][]int{{1, -1}, {1, 1}, {-1, -1}, {-1, 1}}
+	up := [][]int{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+	right := [][]int{{-1, -1}, {1, -1}, {1, 1}, {-1, 1}}
+	left := [][]int{{-1, 1}, {1, 1}, {1, -1}, {-1, -1}}
+	combinations := [][][]int{down, up, right, left}
+	for _, comb := range combinations {
+		func() {
+			for n, side := range comb {
+				if n >= 2 && puzzle[i+side[0]][j+side[1]] != 'S' || n < 2 && puzzle[i+side[0]][j+side[1]] != 'M' {
+					return
+				}
+			}
+			sum++
+		}()
+	}
+
+	return
+}
+
+func read() (puzzle []string) {
 	scanner := util.Scanner("day4")
 	for scanner.Scan() {
 		puzzle = append(puzzle, scanner.Text())
 	}
+	return
+}
 
-	var sum int
-	for i, line := range puzzle {
-		for j, char := range line {
-			if char != 'X' {
-				continue
-			}
-
-			up := [][]int{{1, 0}, {2, 0}, {3, 0}}
-			down := [][]int{{-1, 0}, {-2, 0}, {-3, 0}}
-			left := [][]int{{0, 1}, {0, 2}, {0, 3}}
-			right := [][]int{{0, -1}, {0, -2}, {0, -3}}
-			dgUp := [][]int{{1, 1}, {2, 2}, {3, 3}}
-			dgLeft := [][]int{{1, -1}, {2, -2}, {3, -3}}
-			dgDown := [][]int{{-1, -1}, {-2, -2}, {-3, -3}}
-			dgRight := [][]int{{-1, 1}, {-2, 2}, {-3, 3}}
-			paths := [][][]int{up, down, left, right, dgLeft, dgRight, dgUp, dgDown}
-			for _, path := range paths {
-				sum += isMas(i, j, path, puzzle)
-			}
-		}
+func solvePt1(puzzle []string, i, j int) (sum int) {
+	up := [][]int{{1, 0}, {2, 0}, {3, 0}}
+	down := [][]int{{-1, 0}, {-2, 0}, {-3, 0}}
+	left := [][]int{{0, 1}, {0, 2}, {0, 3}}
+	right := [][]int{{0, -1}, {0, -2}, {0, -3}}
+	dgUp := [][]int{{1, 1}, {2, 2}, {3, 3}}
+	dgLeft := [][]int{{1, -1}, {2, -2}, {3, -3}}
+	dgDown := [][]int{{-1, -1}, {-2, -2}, {-3, -3}}
+	dgRight := [][]int{{-1, 1}, {-2, 2}, {-3, 3}}
+	paths := [][][]int{up, down, left, right, dgLeft, dgRight, dgUp, dgDown}
+	for _, path := range paths {
+		sum += isMas(i, j, path, puzzle)
 	}
-	fmt.Println("Word count:", sum)
+	return
 }
 
 func isMas(i, j int, path [][]int, puzzle []string) int {
